b000499560f3c925914a1a557a7cf2e71528aaf3
Cadence Salinas <77298263+CadenceSalinas@users.noreply.github.com>
2022-06-06 15:33:04 +0000

    added one internship to the list

--- main.sql
+++ main.sql
@@ -8,3 +8,11 @@ CREATE TABLE internships
 	location TEXT,
 	company TEXT
 );
+
+INSERT INTO internships values
+("Concept Artist", 2022, 2022, "lorem ipsum", false, "Irvine, California", "Obsidian Entertainment");
+
+.print
+.print 'Internships list'
+SELECT * FROM internships;
+.print
