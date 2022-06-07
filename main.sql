55f0a353db8cd423cd1ab10fb4e8ae98bc145890
Cadence Salinas <77298263+CadenceSalinas@users.noreply.github.com>
2022-06-07 16:39:19 +0000

    added two more internships

--- main.sql
+++ main.sql
@@ -11,7 +11,9 @@ CREATE TABLE internships
 );
 
 INSERT INTO internships values
-("UX Design Intern", 2022, 2022, "lorem ipsum", false, "San Francisco, California", "Twitch", "BA/BS Graduation Year 2023/2024");
+("UX Design Intern", 2022, 2022, "lorem ipsum", false, "San Francisco, California", "Twitch", "BA/BS Graduation Year 2023/2024"),
+("UX Design Intern", 2022, 2022, "lorem ipsum", false, "Redwood City, California", "Electronic Arts", "Currently in a BS or MS program in Design or related fields"),
+("Game Design Intern", 2022, 2022, "lorem ipsum", true, "Los Angeles, California", "Respawn Entertainment", "Enrolled in a college/university degree program");
 
 .print
 .print 'Internships list'
